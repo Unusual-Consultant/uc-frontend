71bb60266d22be0067149eabcfa6d92dca304cda
Pranav322 <pranav88096@gmail.com>
2025-12-30 17:35:03 +0530

    feat: changed your resume to your resume (matched keywords)

--- app/career-toolkit/components/analysis_results.tsx
+++ app/career-toolkit/components/analysis_results.tsx
@@ -107,7 +107,7 @@ export default function AnalysisResults({
           <div>
             <div className="flex items-center gap-2 mb-2">
               <Image src="/green_tick.png" alt="Your Resume" width={22} height={22} />
-              <h2 className="text-[1.25rem] font-semibold text-gray-900">Your Resume </h2>
+              <h2 className="text-[1.25rem] font-semibold text-gray-900">Your Resume (Matched Keywords)</h2>
             </div>
             <div
               className="bg-[#F8F9FB] border border-gray-300 rounded-xl p-4 mt-2 text-gray-700 text-[0.9375rem] leading-relaxed whitespace-pre-wrap font-[Inter]"
